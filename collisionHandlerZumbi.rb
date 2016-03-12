917dc9b59815cd06b13551520bd3342b0efbd3e6
Ricardo Luiz <ric-luiz@users.noreply.github.com>
2016-03-12 13:44:54 -0300

    Delete collisionHandlerZumbi.rb

--- collisionHandlerZumbi.rb
+++
@@ -1,8 +0,0 @@
-#Classe de manipulação de colisões para o zumbi
-class CollisionHandlerZumbi
-  def begin(a, b, arbiter)
-      if a.object.atacando
-        b.object.atacado = true
-      end
-  end
-end
